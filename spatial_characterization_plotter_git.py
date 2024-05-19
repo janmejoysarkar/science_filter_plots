3eff1a7a185482edc9e092cf902541f47b22a171
janmejoy <janmejoysarker@gmail.com>
2024-05-19 19:05:36 +0530

    Tilt plots generated for all filters. Spatial code modified for NB02. To be implemented for the rest

--- spatial_characterization_plotter_git.py
+++ spatial_characterization_plotter_git.py
@@ -11,28 +11,10 @@ transmission values.
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import glob
 from scipy.interpolate import interp1d
-
-#### Concatenator takes all the single files for spatial transmission and
-#### combines them into one file.
-
-def concatenator(save_name, folder):
-    ls= "*center*", "*top*", "*left*", "*right*", "*bottom*"
-    
-    wl= np.loadtxt(glob.glob(folder+ls[0])[0])[:,0]
-    center= np.loadtxt(glob.glob(folder+ls[0])[0])[:,1]
-    top= np.loadtxt(glob.glob(folder+ls[1])[0])[:,1]
-    left= np.loadtxt(glob.glob(folder+ls[2])[0])[:,1]
-    right= np.loadtxt(glob.glob(folder+ls[3])[0])[:,1]
-    bottom= np.loadtxt(glob.glob(folder+ls[4])[0])[:,1]
-    
-    stack=np.column_stack((wl, center, top, left, right, bottom))
-    #np.savetxt(folder+save_name, stack, delimiter=' ', newline='\n')
-    
-#### Plotter takes a single spatial transmission file and plots it.
+import os
    
-def plotter (filter_name, input_data, wl_min, wl_mx):
+def plotter (filter_name, input_data, wl_min, wl_mx, save_plot=None):
     data=input_data[np.logical_and(input_data[:,0]>wl_min, input_data[:,0]<wl_mx)]
     plt.figure(filter_name, figsize=(6,4))
     plt.plot(data[:,0], data[:,1], color= '#1f77b4')#, label= "Center")
@@ -45,7 +27,7 @@ def plotter (filter_name, input_data, wl_min, wl_mx):
     plt.xlabel("Wavelength (nm)")
     plt.ylabel("Transmission %")
     plt.grid()
-    #plt.savefig("/home/janmejoy/Documents/"+filter_name+"_spatial.png", dpi=300)
+    if save_plot==True: plt.savefig("/home/janmejoy/Documents/"+filter_name+"_spatial.png", dpi=300)
     plt.show()
 
 def deviation(filter_name, input_data, wl_min, wl_mx):
@@ -86,170 +68,101 @@ def deviation(filter_name, input_data, wl_min, wl_mx):
     print("mx_wl-- Mean, Std, %dev:", round(np.mean(wl_mx_ls),2), round(np.std(wl_mx_ls), 2), round(np.std(wl_mx_ls)*100/np.mean(wl_mx_ls), 2))       
     print("fwhm-- Mean, Std, %dev:", round(np.mean(fwhm_ls), 2), round(np.std(fwhm_ls), 2), round(np.std(fwhm_ls)*100/np.mean(fwhm_ls), 2))        
     
+
+if __name__=='__main__':
     
-  
-'''
-### Concatenated file generation ###
-
-# NB5_4 #
-save_name="NB5_4_fm_spatial.txt"
-folder="/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/NB5/NB5_4_FM_spatial/trans_profile/"
-concatenator(save_name, folder)
-
-# NB8_2 #
-save_name="NB8_2_fm_spatial.txt"
-folder="/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/NB8/NB8_2_FM_spatial/trans_profile/"
-concatenator(save_name, folder)
-
-# NB2A_7 #
-save_name="NB2A_7_qm_spatial.txt"
-folder="/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/NB2A/NB2A_7_QM/trans_profile/"
-concatenator(save_name, folder)
-
-# BB1_5 #
-save_name= "BB1_5_fm_spatial.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BB1/BB1_5_QM/trans_profile/"
-concatenator(save_name, folder)
-
-# BB1_3 #
-save_name= "BB1_3_fm_spatial.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BB1/BB1_3_FM/trans_profile/"
-concatenator(save_name, folder)
-
-# BB3_3 #
-save_name= "BB3_3_fm_spatial.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BB3/BB3_3_FM_spatial_OOB/trans_profile/spatial/"
-concatenator(save_name, folder)
-
-# BP3_2 #
-save_name= "BP3_2_fm_spatial_310.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_310/"
-concatenator(save_name, folder)
-save_name= "BP3_2_fm_spatial_350.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_350/"
-concatenator(save_name, folder)
-save_name= "BP3_2_fm_spatial_390.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_390/"
-concatenator(save_name, folder)
-
-# BP4_4 #
-save_name= "BP4_4_fm_spatial_270.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP4/BP4_4_FM_spatial/trans_profile/spatial_270/"
-concatenator(save_name, folder)
-save_name= "BP4_4_fm_spatial_310.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP4/BP4_4_FM_spatial/trans_profile/spatial_310/"
-concatenator(save_name, folder)
-save_name= "BP4_4_fm_spatial_350.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BP4/BP4_4_FM_spatial/trans_profile/spatial_350/"
-concatenator(save_name, folder)
-
-# NB1_1 #
-save_name= "NB1_1_fm_spatial_214.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_214/"
-concatenator(save_name, folder)
-save_name= "NB1_1_fm_spatial_254.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_254/"
-concatenator(save_name, folder)
-
-# BB2_3 # 
-save_name= "BB2_3_fm_spatial_255.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_255/"
-concatenator(save_name, folder)
-save_name= "BB2_3_fm_spatial_290.txt"
-folder= "/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_290/"
-concatenator(save_name, folder)
-'''
-#####################################################################
-
-### Plotting & deviation analysis ###
+    ### Plotting & deviation analysis ###
+        
+    folder=os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/science_filter_charactrerization_scripts/science_filter_plots_project/')
     
-folder="/home/janmejoy/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/"
-
-# NB2A_7 #
-file= folder+ 'NB2A/NB2A_7_QM/trans_profile/NB2A_7_qm_spatial.txt' 
-plotter("NB2A_7", np.loadtxt(file, skiprows=1), 275, 279)
-deviation("NB2A_7", np.loadtxt(file, skiprows=1), 275, 279)
-
-# NB3_3 #
-file= folder+'NB3/NB3_3_FM/trans_profile/NB3_3_fm_spatial.txt'
-plotter("NB3_3", np.loadtxt(file, skiprows=1), 278.5, 281.5)
-deviation("NB3_3", np.loadtxt(file, skiprows=1), 278.5, 281.5)
-
-# NB4_2 #
-file= folder+ 'NB4/NB4_2_QM/trans_profile/NB4_2_qm_spatial_oob.txt'
-plotter("NB4_2", np.loadtxt(file, skiprows=1), 278.5, 283)
-deviation("NB4_2", np.loadtxt(file, skiprows=1), 278.5, 283)
-
-# NB5_4 #
-file= folder+'NB5/NB5_4_FM_spatial/trans_profile/NB5_4_fm_spatial.txt'
-plotter("NB5_4", np.loadtxt(file, skiprows=1), 280.5, 286)
-deviation("NB5_4", np.loadtxt(file, skiprows=1), 280.5, 286)
-
-# NB6_3 #
-file= folder+ 'NB6/NB6_3_spatial_FM/trans_profile/NB6_3_fm_spatial.txt'
-plotter("NB6_3", np.loadtxt(file, skiprows=1), 296, 304)
-deviation("NB6_3", np.loadtxt(file, skiprows=1), 296, 304)
-
-# NB7_1 #
-file= folder+ 'NB7/NB7_1_FM/trans_profile/NB7_1_fm_spatial.txt'
-plotter("NB7_1", np.loadtxt(file, skiprows=1), 385, 391)
-deviation("NB7_1", np.loadtxt(file, skiprows=1), 385, 391)
-
-# NB8_1 #
-file= folder+ 'NB8/NB8_1_spatial/trans_profile/NB8_1_qm_spatial_oob.txt'
-plotter("NB8_1", np.loadtxt(file, skiprows=1), 396.25, 397.25)
-deviation("NB8_1", np.loadtxt(file, skiprows=1), 396.25, 397.25)
-
-# NB8_2 #
-file= folder+ 'NB8/NB8_2_FM_spatial/trans_profile/NB8_2_fm_spatial.txt'
-plotter("NB8_2", np.loadtxt(file, skiprows=1), 396.25, 397.25)
-deviation("NB8_2", np.loadtxt(file, skiprows=1), 396.25, 397.25)
-
-# BB1_5 #
-file= folder+'BB1/BB1_5_QM/trans_profile/BB1_5_fm_spatial.txt'
-plotter("BB1_5", np.loadtxt(file, skiprows=1), 100, 400)
-
-# BB1_3 #
-file= folder+'BB1/BB1_3_FM/trans_profile/BB1_3_fm_spatial.txt'
-plotter("BB1_3", np.loadtxt(file, skiprows=1), 100, 400)
-
-# BB3_3 #
-file= folder+"BB3/BB3_3_FM_spatial_OOB/trans_profile/spatial/BB3_3_fm_spatial.txt"
-plotter("BB3_3", np.loadtxt(file, skiprows=1), 100, 400)
-
-# BP2_8 #
-file= folder+"BP2/BP2_8_Qm/trans_profile/BP2_8_qm_spatial.txt"
-plotter("BP2_8", np.loadtxt(file, skiprows=1), 265, 295)
-
-# BP3_2 #
-file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_310/BP3_2_fm_spatial_310.txt"
-plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 400)
-
-file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_350/BP3_2_fm_spatial_350.txt"
-plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 400)
-
-file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_390/BP3_2_fm_spatial_390.txt"
-plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 500)
-
-# BP4_4 #
-file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_270/BP4_4_fm_spatial_270.txt"
-plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 400)
-
-file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_310/BP4_4_fm_spatial_310.txt"
-plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 400)
-
-file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_350/BP4_4_fm_spatial_350.txt"
-plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 500)
-
-# NB1_1 #
-file= folder+"NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_214/NB1_1_fm_spatial_214.txt"
-plotter("NB1_1", np.loadtxt(file, skiprows=1), 180, 400)
-
-file= folder+"NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_254/NB1_1_fm_spatial_254.txt"
-plotter("NB1_1", np.loadtxt(file, skiprows=1), 180, 400)
-
-# BB2_3 #
-file= folder+"BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_255/BB2_3_fm_spatial_255.txt"
-plotter("BB2_3", np.loadtxt(file, skiprows=1), 200, 400)
-file= folder+"BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_290/BB2_3_fm_spatial_290.txt"
-plotter("BB2_3", np.loadtxt(file, skiprows=1), 276, 400)
+    # NB2A_7 #
+    ftr_name= "NB02"
+    file= f'{folder}data/processed/NB02/spatial/NB2A_7_spatial.txt' 
+    wl_mn, wl_mx= 275, 279
+    plotter(ftr_name, np.loadtxt(file, skiprows=1), wl_mn, wl_mx, save_plot=True)
+    deviation(ftr_name, np.loadtxt(file, skiprows=1),  wl_mn, wl_mx)
+    '''
+    # NB3_3 #
+    file= folder+'NB3/NB3_3_FM/trans_profile/NB3_3_fm_spatial.txt'
+    plotter("NB3_3", np.loadtxt(file, skiprows=1), 278.5, 281.5)
+    deviation("NB3_3", np.loadtxt(file, skiprows=1), 278.5, 281.5)
+    
+    # NB4_2 #
+    file= folder+ 'NB4/NB4_2_QM/trans_profile/NB4_2_qm_spatial_oob.txt'
+    plotter("NB4_2", np.loadtxt(file, skiprows=1), 278.5, 283)
+    deviation("NB4_2", np.loadtxt(file, skiprows=1), 278.5, 283)
+    
+    # NB5_4 #
+    file= folder+'NB5/NB5_4_FM_spatial/trans_profile/NB5_4_fm_spatial.txt'
+    plotter("NB5_4", np.loadtxt(file, skiprows=1), 280.5, 286)
+    deviation("NB5_4", np.loadtxt(file, skiprows=1), 280.5, 286)
+    
+    # NB6_3 #
+    file= folder+ 'NB6/NB6_3_spatial_FM/trans_profile/NB6_3_fm_spatial.txt'
+    plotter("NB6_3", np.loadtxt(file, skiprows=1), 296, 304)
+    deviation("NB6_3", np.loadtxt(file, skiprows=1), 296, 304)
+    
+    # NB7_1 #
+    file= folder+ 'NB7/NB7_1_FM/trans_profile/NB7_1_fm_spatial.txt'
+    plotter("NB7_1", np.loadtxt(file, skiprows=1), 385, 391)
+    deviation("NB7_1", np.loadtxt(file, skiprows=1), 385, 391)
+    
+    # NB8_1 #
+    file= folder+ 'NB8/NB8_1_spatial/trans_profile/NB8_1_qm_spatial_oob.txt'
+    plotter("NB8_1", np.loadtxt(file, skiprows=1), 396.25, 397.25)
+    deviation("NB8_1", np.loadtxt(file, skiprows=1), 396.25, 397.25)
+    
+    # NB8_2 #
+    file= folder+ 'NB8/NB8_2_FM_spatial/trans_profile/NB8_2_fm_spatial.txt'
+    plotter("NB8_2", np.loadtxt(file, skiprows=1), 396.25, 397.25)
+    deviation("NB8_2", np.loadtxt(file, skiprows=1), 396.25, 397.25)
+    
+    # BB1_5 #
+    file= folder+'BB1/BB1_5_QM/trans_profile/BB1_5_fm_spatial.txt'
+    plotter("BB1_5", np.loadtxt(file, skiprows=1), 100, 400)
+    
+    # BB1_3 #
+    file= folder+'BB1/BB1_3_FM/trans_profile/BB1_3_fm_spatial.txt'
+    plotter("BB1_3", np.loadtxt(file, skiprows=1), 100, 400)
+    
+    # BB3_3 #
+    file= folder+"BB3/BB3_3_FM_spatial_OOB/trans_profile/spatial/BB3_3_fm_spatial.txt"
+    plotter("BB3_3", np.loadtxt(file, skiprows=1), 100, 400)
+    
+    # BP2_8 #
+    file= folder+"BP2/BP2_8_Qm/trans_profile/BP2_8_qm_spatial.txt"
+    plotter("BP2_8", np.loadtxt(file, skiprows=1), 265, 295)
+    
+    # BP3_2 #
+    file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_310/BP3_2_fm_spatial_310.txt"
+    plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 400)
+    
+    file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_350/BP3_2_fm_spatial_350.txt"
+    plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 400)
+    
+    file= folder+"BP3/BP3_2_FM_Spatial_OOB/trans_profile/spatial_390/BP3_2_fm_spatial_390.txt"
+    plotter("BP3_2", np.loadtxt(file, skiprows=1), 200, 500)
+    
+    # BP4_4 #
+    file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_270/BP4_4_fm_spatial_270.txt"
+    plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 400)
+    
+    file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_310/BP4_4_fm_spatial_310.txt"
+    plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 400)
+    
+    file= folder+"BP4/BP4_4_FM_spatial/trans_profile/spatial_350/BP4_4_fm_spatial_350.txt"
+    plotter("BP4_4", np.loadtxt(file, skiprows=1), 200, 500)
+    
+    # NB1_1 #
+    file= folder+"NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_214/NB1_1_fm_spatial_214.txt"
+    plotter("NB1_1", np.loadtxt(file, skiprows=1), 180, 400)
+    
+    file= folder+"NB1/NB1_1_FM_spatial/trans_profile/NB1_1_spatial_254/NB1_1_fm_spatial_254.txt"
+    plotter("NB1_1", np.loadtxt(file, skiprows=1), 180, 400)
+    
+    # BB2_3 #
+    file= folder+"BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_255/BB2_3_fm_spatial_255.txt"
+    plotter("BB2_3", np.loadtxt(file, skiprows=1), 200, 400)
+    file= folder+"BB2/BB2_3_FM_spatial_OOB/trans_profile/spatial_290/BB2_3_fm_spatial_290.txt"
+    plotter("BB2_3", np.loadtxt(file, skiprows=1), 276, 400)
+    '''
--- tilt_characterization_plotter.py
+++ tilt_characterization_plotter.py
@@ -10,74 +10,66 @@ Created on Mon Oct 30 12:04:00 2023
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
+import os
 
-
-def tilt_plotter(folder, wl_min, wl_max, target, filter_name):
-    ls=sorted(glob.glob(path+folder+'*deg*'))
+def tilt_plotter(file, wl_min, wl_max, target, filter_name, saveplot=None):
+    data=np.loadtxt(file, skiprows=1)
+    plot_data=data[np.logical_and(data[:,0]>wl_min, data[:,0]<wl_max)] #selects the requisite wavelength
     plt.figure(figsize=(6,4))
-    for i in ls:
-        data=np.loadtxt(i)
-        plot_data=data[np.logical_and(data[:,0]>wl_min, data[:,0]<wl_max)] #selects the requisite wavelength
-        wl=plot_data[:,0]
-        plt.plot(wl, plot_data[:,1]*100)
+    for i in range(7,13):
+        plt.plot(plot_data[:,0], plot_data[:,i])
     plt.axvline(target, color='black')
     plt.legend(["0°", "1°", "2°", "3°", "4°", "5°", "6°", 'Central λ'])
     plt.xlabel('Wavelength (nm)')
     plt.ylabel('% Transmission')
     plt.title(filter_name+'_tilt | λ= '+str(target))
     plt.grid()
-    plt.savefig('/home/janmejoy/Documents/'+filter_name+'_tilt.png', dpi=300)
+    if saveplot==True: plt.savefig(f'{project_path}products/tilt/{filter_name}_tilt.pdf', dpi=300)
     plt.show()
 
-#global common path to the data
-path='/home/janmejoyarch/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/results/filter_data_compiled/'
+if __name__=='__main__':
 
-### NB1_2_QM_tilt ###
-folder='NB1/NB1_2_QM_tilt/trans_profile/'
-wl_min= 200
-wl_max= 240
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 214, filter_name)
-
-### NB2A_7_QM_tilt ###
-folder='NB2A/NB2A_7_QM_tilt/trans_profile/'
-wl_min= 275
-wl_max= 279
-filter_name= folder[5:11]
-tilt_plotter(folder, wl_min, wl_max, 276.6, filter_name)
-
-### NB3_2_QM_tilt ###
-folder='NB3/NB3_2_QM_tilt/trans_profile/'
-wl_min= 278.5
-wl_max= 281.5
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 279.6, filter_name)
-
-### NB4_2_QM_tilt ###
-folder='NB4/NB4_2_QM_tilt/trans_profile/'
-wl_min= 278.5
-wl_max= 283
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 280.3, filter_name)
-
-### NB5_3_QM_tilt ###
-folder='NB5/NB5_3_QM_tilt/trans_profile/'
-wl_min= 280.5
-wl_max= 286
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 283.2, filter_name)
-
-### NB6_1_QM_tilt ###
-folder='NB6/NB6_1_QM_tilt/trans_profile/'
-wl_min= 296
-wl_max= 304
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 300, filter_name)
-
-### NB7_2_QM_tilt ###
-folder='NB7/NB7_2_QM_tilt/trans_profile/'
-wl_min= 385
-wl_max= 391
-filter_name= folder[4:9]
-tilt_plotter(folder, wl_min, wl_max, 388, filter_name)
+    #global common path to the data
+    project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/science_filter_charactrerization_scripts/science_filter_plots_project/')
+    
+    ### NB1_2_QM_tilt ###
+    filter_name= 'NB01' #CHECK
+    wl_min, wl_max, cwl= 200, 240, 240
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB1_1_tilt.txt'
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name)
+    
+    ### NB2A_7_QM_tilt ###
+    filter_name= 'NB02'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB2A_7_tilt.txt'
+    wl_min, wl_max, cwl= 275, 279, 276.6
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
+    
+    ### NB3_2_QM_tilt ###
+    filter_name= 'NB03'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB3_2_tilt.txt'
+    wl_min, wl_max, cwl= 278.5, 281.5, 279.6
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
+    
+    ### NB4_2_QM_tilt ###
+    filter_name= 'NB04'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB4_2_tilt.txt'
+    wl_min, wl_max, cwl= 278.5, 283, 280.3
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
+    
+    ### NB5_3_QM_tilt ###
+    filter_name= 'NB05'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB5_3_tilt.txt'
+    wl_min, wl_max, cwl= 280.5, 286, 283.2
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
+    
+    ### NB6_1_QM_tilt ###
+    filter_name= 'NB06'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB6_1_tilt.txt'
+    wl_min, wl_max, cwl= 296, 304, 300
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
+    
+    ### NB7_2_QM_tilt ###
+    filter_name= 'NB07'
+    file= f'{project_path}data/processed/{filter_name}/tilt/NB7_2_tilt.txt'
+    wl_min, wl_max, cwl= 385, 391, 388
+    tilt_plotter(file, wl_min, wl_max, cwl, filter_name, saveplot=True)
