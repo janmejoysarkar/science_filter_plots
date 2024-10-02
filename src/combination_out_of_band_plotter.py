6133f1bee5a6bed2989ebc750b537400390195b2
janmejoy <janmejoysarker@gmail.com>
2024-10-02 19:43:02 +0530

    Created combined transmission profile for BB02

--- src/combination_out_of_band_plotter.py
+++ src/combination_out_of_band_plotter.py
@@ -51,7 +51,7 @@ def integrate(fill_ib, fill_oob_r, fill_oob_b):
     ib_integrate= simpson(fill_ib[:,1],x=fill_ib[:,0])
     oob_red_integrate= simpson(fill_oob_r[:,1], x=fill_oob_r[:,0])
     oob_red_percent= oob_red_integrate*100/ib_integrate
-    if fill_oob_b is not None:
+    if len (fill_oob_b) != 0: #NOTE: This is condition is different from the oob tx code 
         oob_blue_integrate= simpson(fill_oob_b[:,1], x=fill_oob_b[:,0])
         oob_blue_percent= oob_blue_integrate*100/ib_integrate
     else:
@@ -85,8 +85,7 @@ def combined(FILT1, FILT2, lamda1, lamda2, fill_width, cent=None):
     final= np.column_stack((x_new, y_new_1*y_new_2)) #Wavelength vs filt1*filt2 tx% array
     
     fill_ib= fill_interval(final, fill_width, cent) #IB fill area
-    if len(FILT1[1])!=0:
-        fill_oob_b= fill_interval(final, fill_width, FILT1[1][int(len(FILT1[1])/2)][0]) #OOB Blue fill area
+    fill_oob_b= fill_interval(final, fill_width, FILT1[1][int(len(FILT1[1])/2)][0]) #OOB Blue fill area
     fill_oob_r= fill_interval(final, fill_width, FILT1[2][int(len(FILT1[2])/2)][0]) #OOB Red fill area
     integrate(fill_ib, fill_oob_r, fill_oob_b) #Integration across fill area
     #Plotting 
@@ -97,8 +96,7 @@ def combined(FILT1, FILT2, lamda1, lamda2, fill_width, cent=None):
                  [(final[:,0]>FILT1[0][:,0][0]) & (final[:,0]<FILT1[0][:,0][-1])], color= 'red', label='Inband')
         plt.fill_between(fill_ib[:,0],fill_ib[:,1], y2=0, linewidth=0, color='red', alpha=0.2)
         plt.fill_between(fill_oob_r[:,0], fill_oob_r[:,1], y2=0, linewidth=0, color='black', alpha=0.2)
-        if len(FILT1[1])!=0:
-            plt.fill_between(fill_oob_b[:,0], fill_oob_b[:,1], y2=0, linewidth=0, color='black', alpha=0.2)
+        plt.fill_between(fill_oob_b[:,0], fill_oob_b[:,1], y2=0, linewidth=0, color='black', alpha=0.2)
         #plt.yscale('log')
         plt.xlabel('Wavelength (nm)', fontsize=12)
         plt.ylabel('Relative Transmission', fontsize=12)
@@ -111,7 +109,7 @@ def combined(FILT1, FILT2, lamda1, lamda2, fill_width, cent=None):
         if SHOW: plt.show()
 
 if __name__=='__main__':
-    SHOW, SAVE= True, False
+    SHOW, SAVE= False, True
     project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/science_filter_characterization/science_filter_charactrerization_scripts/science_filter_plots_project/') 
     folder= os.path.join(project_path, 'data/processed/')
     print("OOB %tx wrt IB")
@@ -229,4 +227,8 @@ if __name__=='__main__':
     filt_name="NB07"; combined(NB07, BP03, int(NB07[1][0][0])+1, int(NB07[2][-1][0]), 1, 388)
     filt_name="BB01"; combined(BB01, BB01, int(BB01[1][0][0])+1, int(BB01[2][-1][0]), 5, BB01[0][int(len(BB01[0])/2)][0])
     filt_name="BB03"; combined(BB03, BP04, 293, int(BB03[2][-1][0]), 5, BB03[0][int(len(BB03[0])/2)][0])
-    filt_name="BB02"; combined((BB02[0][2250:],np.empty(shape=(0,2)), BB02[2]), BP04, 293, int(BB02[2][-1][0]), 5, cent=None)
+    #BB02 is a special case as the blue end of the BB02 ib and oob_b spectra does not have
+    #any overlap with the available BP04 spectra. Therefore, the central ib wl is forcefully
+    #set to 295nm with a width of +-1nm.
+    filt_name="BB02"; combined(BB02, BP04, 293, int(BB02[2][-1][0]), 1, 295)
+
